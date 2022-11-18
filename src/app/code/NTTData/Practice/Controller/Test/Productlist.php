493e641d82163d9bbb3e908f3e732d6ff4bd8faf
mteran-nttdata <mteranfu@emeal.nttdata.com>
2022-11-18 17:05:17 -0300

    revision del ejercicio-03

--- src/app/code/NTTData/Practice/Controller/Test/Productlist.php
+++ src/app/code/NTTData/Practice/Controller/Test/Productlist.php
@@ -16,29 +16,16 @@ class Productlist extends Action
 		\Magento\Framework\App\Action\Context $context,
 		\Magento\Framework\View\Result\PageFactory $pageFactory)
 	{
-		$this->_pageFactory = $pageFactory;
+		 $this->_pageFactory = $pageFactory; 
 		return parent::__construct($context);
 	}
 
 	public function execute()
 	{
-
-		$objectManager =  \Magento\Framework\App\ObjectManager::getInstance();        
-		$storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
-		$storeID= $storeManager->getStore()->getStoreId(); 
-
-		if ($storeID == 3) {
-			$date = date_default_timezone_set("America/Argentina/Buenos_Aires");
-			$date = date("H:i:s");
-			
-		}else {
-			$date = date_default_timezone_set("America/New_York");
-			$date = date("H:i:s");
-			
-		}
-
+		
 		$pagefactory = $this->_pageFactory->create();
-		$pagefactory->getConfig()->getTitle()->set(__("Now being %1, I'm learning translations", $date ));
 		return $pagefactory;
 	}
+
+
 }

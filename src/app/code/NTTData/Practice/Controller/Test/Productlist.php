85f3d9b07df6626e6071544fcd7918d32b4d2535
mteran-nttdata <mteranfu@emeal.nttdata.com>
2022-11-22 15:43:05 -0300

    remove use object manager in Controller

--- src/app/code/NTTData/Practice/Controller/Test/Productlist.php
+++ src/app/code/NTTData/Practice/Controller/Test/Productlist.php
@@ -2,28 +2,41 @@
 
 namespace NTTData\Practice\Controller\Test;
 
-use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Action\Action;
-use Magento\Framework\Controller\Result\JsonFactory;
-
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Store\Model\StoreManagerInterface;
 
 
 class Productlist extends Action
 {
     protected $_pageFactory;
+	protected $storeManager;
 
 	public function __construct(
-		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
+		Context $context,
+		PageFactory $pageFactory,
+		StoreManagerInterface $storeManager)
 	{
-		 $this->_pageFactory = $pageFactory; 
+		$this->_pageFactory = $pageFactory; 
+		$this->storeManager = $storeManager;
 		return parent::__construct($context);
 	}
 
 	public function execute()
 	{
 		
+		$storeID= $this->storeManager->getStore()->getId(); 
+		
+		date_default_timezone_set("America/New_York");
+		if ($storeID == 3) {
+			date_default_timezone_set("America/Argentina/Buenos_Aires");
+		}
+
+
+		$date = date("H:i:s");
 		$pagefactory = $this->_pageFactory->create();
+		$pagefactory->getConfig()->getTitle()->set(__("Now being %1, I'm learning translations", $date ));
 		return $pagefactory;
 	}
 
